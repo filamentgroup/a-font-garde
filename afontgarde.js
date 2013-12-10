6ef6315200d165549c8574dd1c07930ca37d8016
Zach Leatherman <zachleatherman@gmail.com>
2013-12-10 11:44:27 -0600

    Simpler implementation

---
+++ afontgarde.js
@@ -0,0 +1,61 @@
+;(function( w ) {
+
+	var doc = w.document,
+		ref,
+		css = ['.supports-fontface.supports-generatedcontent.%s .icon-fallback-text .icon { display: inline-block; }',
+			'.supports-fontface.supports-generatedcontent.%s .icon-fallback-text label { clip: rect(0 0 0 0); overflow: hidden; position: absolute; height: 1px; width: 1px; }',
+			'.supports-fontface.%s .icon-fallback-glyph .icon:before { font-size: inherit; line-height: inherit; }'];
+
+	function init( fontFamily, sampleGlyphs) {
+		
+	}
+
+	function addEvent( type, callback ) {
+		if( 'addEventListener' in w ) {
+			return w.addEventListener( type, callback, false );
+		} else if( 'attachEvent' in w ) {
+			return w.attachEvent( 'on' + type, callback );
+		}
+	}
+
+	AFontGarde = function( fontFamily, sampleGlyphs ) {
+		var executed = false;
+		function init() {
+			if( executed ) {
+				return;
+			}
+			executed = true;
+
+			if( typeof FontFaceOnload === 'undefined' ) {
+				throw 'FontFaceOnload is a prerequisite.';
+			}
+
+			if( !ref ) {
+				ref = doc.getElementsByTagName( 'script' )[ 0 ];
+			}
+			var style = doc.createElement( 'style' );
+			style.innerHTML = css.join( '\n' ).replace( /\%s/gi, fontFamily );
+			ref.parentNode.insertBefore( style, ref );
+
+			FontFaceOnload( fontFamily, {
+				// These characters are a few of the glyphs from the font above */
+				glyphs: sampleGlyphs || '',
+				timeout: 5000,
+				success: function() {
+					// If you’re using more than one icon font, change this classname (and in a-font-garde.css)
+					doc.documentElement.className += ' ' + fontFamily;
+				}
+			});
+		}
+
+		// MIT credit: filamentgroup/shoestring
+		addEvent( "DOMContentLoaded", init );
+		addEvent( "readystatechange", init );
+		addEvent( "load", init );
+
+		if( w.document.readyState === "complete" ){
+			init();
+		}
+	};
+
+})( this );
